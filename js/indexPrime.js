305b062c2494817cb4852563e9b171aa8c51a4f0
christiancampero <68880380+christiancampero@users.noreply.github.com>
2020-08-29 22:31:15 -0300

    Changes were made to products and login system
    Se realizaron cambios en products para cumplir con la consigna de la entrega 2. También se agrego información sobre el usuario conectado, en la barra de "info usuario"

--- js/indexPrime.js
+++ js/indexPrime.js
@@ -1,9 +1,15 @@
 //la siguiente funcion muestra el usuario que ingreso actualmente y que esta almacenado
 //en localStorage. Se puede visualizar en la navBar o barra de navegacion, especificamente donde dice "Info Usuario:"
+//tambien se muestra la cuenta de google actualmente conectada
 function infoUSuarioBarra() {
 
     var nuevito = JSON.parse(localStorage.getItem('quienIngreso'));
-    document.getElementById("usuarioON").innerHTML = nuevito + `<p>actualmente conectado</p>`; 
+    document.getElementById("usuarioON").innerHTML = nuevito + `<p>actualmente conectado/a</p>`; 
+
+    var nuevitoGoog = JSON.parse(localStorage.getItem('entidadUsuario'));
+    
+    console.log(nuevitoGoog);
+    document.getElementById("usuarioGoog").innerHTML = nuevitoGoog.Name + `<p>conectado/a con cuenta Google</p>`;
 
 }
 
