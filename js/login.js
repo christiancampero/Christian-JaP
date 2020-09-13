cb4d779230248dadf8b0f00a71befdc668cc8cb9
christiancampero <68880380+christiancampero@users.noreply.github.com>
2020-09-12 21:11:07 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -75,12 +75,6 @@ function solucionarProblemas(un, pw){
 };
  */
 
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-/*document.addEventListener("DOMContentLoaded", function(e){
-});*/
-
 
 //codigo por defecto de google developers
 var googleUser;
