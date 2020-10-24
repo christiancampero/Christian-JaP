0c112edb3948c183516b9be1c6fcf04b36e1da06
christiancampero <68880380+christiancampero@users.noreply.github.com>
2020-10-23 23:37:56 -0300

    Updated cart.js

--- js/cart.js
+++ js/cart.js
@@ -4,9 +4,13 @@ const urlMensaje = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 const currencyRates = "https://api.currencyfreaks.com/latest?apikey=cc9ff3a12c0d485583362e12d645dee2";
 const allProducts = "https://japdevdep.github.io/ecommerce-api/product/all.json";
 
-var arrayUnido = {};
-var articulos = {};
-var productos = {};
+
+let dolarAPeso = 43;
+var contenedor = [];
+
+var arrayMini = [];
+var articulos = [];
+var productos = [];
 let porcentajeEnvio = 0.15;
 var contador = 0;
 var subTotal = 0;
@@ -18,6 +22,7 @@ var subTotalRemovido = 0;
 let monedaProductoRemovido = "";
 var costoUnitarioProductoRemovido = 0;
 
+let infoFaltante = true;
 let metodoPagoSeleccionado = false;
 const tarjeta_credito = "Tarjeta de crédito";
 const pago_bancario = "Transferencia bancaria";
@@ -35,13 +40,15 @@ function actualizarIntermedio() {
 
     var cartItems = document.querySelectorAll('.costos');
     var totalItems = 0;
-    
+    contador = document.getElementsByName("canti");
+
     for (let i = 0; i < cartItems.length; i++) {
+
         var cartI = cartItems[i];
-        contador = document.getElementById(`controlador${i}`).value
+        let quanti = contador[i]; 
         var price = parseInt(cartI.innerText);
       
-        totalItems = totalItems + (price * contador);
+        totalItems = totalItems + (price * quanti.value);
         Total = totalItems;
         actualizarCostosTotales();
     }
@@ -67,7 +74,7 @@ function mostrarMetodoDePago(){
 
 }
 
-function ocultarMetodoDePago(){
+function ocultarMetodoDePagoNoSeleccionado(){
     document.getElementById("metodo-pago").style.display = "none";
 
 }
@@ -83,7 +90,7 @@ function mostrarArticulos(array){
         if(element.currency === "USD") {
                 
             monedaProducto = "UYU";
-            costoUnitarioProducto = (element.unitCost * 43);
+            costoUnitarioProducto = (element.unitCost * dolarAPeso);
 
         } else if(element.currency === "UYU") { 
              
@@ -92,14 +99,14 @@ function mostrarArticulos(array){
         }
 
         htmlContentToAppend += `
-        <tr id="linea${i}">
+        <tr class="varios" id="linea${i}">
             <td><img src='${element.src}' width="85px"></td>
             <td>${element.name}</td>
             <td>${monedaProducto}</td>
             <td class="costos" id="costoU${i}">${costoUnitarioProducto}</td>
-            <td><input class="form-control" style="width:60px;" id="controlador${i}" type="number" value="${element.count}" min="1"></td>
+            <td><input class="form-control" name="canti" style="width:60px;" id="controlador${i}" type="number" value="${element.count}" min="1"></td>
             <td><span class="cambiador" id="precio${i}" style="font-weight:bold;">${costoUnitarioProducto * element.count}</span></td>
-            <td><button type="button" class="btn btn-danger" id="borrar${i}">Quitar</button></td>
+            <td id="tdBorrar${i}"><button type="button" class="btn btn-danger" id="borrar${i}">Quitar</button></td>
         </tr>
         `;
     }
@@ -116,7 +123,7 @@ function mostrarArticulos(array){
             if(item.currency === "USD") {
 
                 monedaProducto = "UYU";
-                costoUnitarioProducto = (item.unitCost * 43);
+                costoUnitarioProducto = (item.unitCost * dolarAPeso);
     
             } else if(item.currency === "UYU") { 
                  
@@ -129,7 +136,7 @@ function mostrarArticulos(array){
             actualizarSubTotal(i); 
             actualizarIntermedio();
         });
-    }
+    }    
     removerArticulo(array);
 }
 
@@ -140,16 +147,24 @@ document.addEventListener("DOMContentLoaded", function(){
     obtenerJSONData(urlCarrito).then(respuesta => {
         
         articulos = respuesta.all.articles;
-        mostrarArticulos(articulos);
-        actualizarCostosTotales();
         
         obtenerJSONData(allProducts).then(result => {
  
             productos = result.all;
             generarNuevoProducto(productos, articulos);
-            mostrarArticulos(articulos)
+            mostrarArticulos(articulos);
             actualizarCostosTotales();
+
+            document.getElementById("recargar").addEventListener("click", event =>{
+
+                mostrarArticulos(articulos);
+            });
         });
+    });
+
+    document.getElementById("limpiar").addEventListener("click", event => {
+
+        document.getElementById("mostrarArticulos-removidos").innerHTML = "";        
     });
     
     document.getElementById("radio-premium").addEventListener("change", function(){
@@ -180,10 +195,51 @@ document.addEventListener("DOMContentLoaded", function(){
         document.getElementById("paymentType").innerHTML = tarjeta_credito;
 
         metodoPagoSeleccionado = true;
-        ocultarMetodoDePago();
+        ocultarMetodoDePagoNoSeleccionado();
+
+        document.getElementById("creditCardNumber").addEventListener("keyup", event =>{
+
+            let cNumber = document.getElementById("creditCardNumber").value;
+
+            if(cNumber.length < 14) {
+                infoFaltante = true;
+                document.getElementById("creditCardNumber").classList.add('is-invalid');
+            } else if(cNumber.length >= 14) {
+                infoFaltante = false;
+                document.getElementById("creditCardNumber").classList.remove('is-invalid');
+            }
+        });
+
+        document.getElementById("creditCardSecurityCode").addEventListener("keyup", event =>{
+
+            let cSecurity = document.getElementById("creditCardSecurityCode").value;
+
+            if(cSecurity.length < 3) {
+                infoFaltante = true;
+                document.getElementById("creditCardSecurityCode").classList.add('is-invalid');
+            } else if(cSecurity.length >= 3) {
+                infoFaltante = false;
+                document.getElementById("creditCardSecurityCode").classList.remove('is-invalid');
+            }
+        });
+
+        document.getElementById("dueDate").addEventListener("keyup", event =>{
+
+            let dDate = document.getElementById("dueDate").value;
+
+            if(dateFormat(dDate)=== false) {
+                infoFaltante = true;
+                document.getElementById("dueDate").classList.add('is-invalid');
+            } else if(dateFormat(dDate) === true) {
+                infoFaltante = false;
+                document.getElementById("dueDate").classList.remove('is-invalid');
+            }
+        });
+
     });
 
     document.getElementById("bankingRadio").addEventListener("change", function(){
+        
         document.getElementById("bankAccountNumber").disabled = false;
 
         document.getElementById("creditCardNumber").disabled = true;
@@ -191,9 +247,24 @@ document.addEventListener("DOMContentLoaded", function(){
         document.getElementById("dueDate").disabled = true;
 
         document.getElementById("paymentType").innerHTML = pago_bancario;
-
+        
         metodoPagoSeleccionado = true;
-        ocultarMetodoDePago();
+        ocultarMetodoDePagoNoSeleccionado();
+
+        document.getElementById("bankAccountNumber").addEventListener("keyup", event => {
+            
+            let bANumber = document.getElementById("bankAccountNumber").value;
+
+            if(bANumber.length < 14 || emptyString(bANumber) === false){
+                infoFaltante = true;
+                document.getElementById("bankAccountNumber").classList.add('is-invalid');
+
+            } else if(bANumber.length >= 14 || emptyString(bANumber) === true){
+
+                document.getElementById("bankAccountNumber").classList.remove('is-invalid');
+                infoFaltante = false;
+            }
+        });
     });
 
     
@@ -204,34 +275,47 @@ document.addEventListener("DOMContentLoaded", function(){
         let shippingStreetInput = document.getElementById("shippingStreet");
         let shippingStreetNumberInput = document.getElementById("shippingStreetNumber");
         let shippingCornerStreetInput = document.getElementById("shippingCornerStreet");
-        let infoFaltante = false;
+        infoFaltante = false;
 
-        ocultarMetodoDePago(); 
+        ocultarMetodoDePagoNoSeleccionado(); 
 
         shippingStreetInput.classList.remove('is-invalid');
         shippingStreetNumberInput.classList.remove('is-invalid');
         shippingCornerStreetInput.classList.remove('is-invalid');
 
-        
-        if (shippingStreetInput.value === "")
+        if(shippingStreetInput.value === "" || hasNumber(shippingStreetInput.value)===true || streetNameWithSpaces(shippingStreetInput)=== true)
         {
             shippingStreetInput.classList.add('is-invalid');
             infoFaltante = true;
-        }
+            alert("El nombre de la calle no puede contener números. Introduce el número en el campo correspondiente");
 
-        if (shippingStreetNumberInput.value === "")
+        } else if(shippingStreetInput.value != "" && hasNumber(shippingStreetInput.value) === false) {
+            shippingStreetInput.classList.remove('is-invalid');
+            infoFaltante = false;
+        }
+                
+        if (shippingStreetNumberInput.value === "" || shippingStreetNumberInput.value.length < 4 || onlyNumbers(shippingStreetNumberInput.value)===false)
         {
             shippingStreetNumberInput.classList.add('is-invalid');
             infoFaltante = true;
+            alert("El número de calle/casa debe contener por lo menos 4 digitos o más");
+
+        } else if(shippingStreetNumberInput.value.length > 4 && shippingStreetNumberInput != "" && onlyNumbers(shippingStreetNumberInput.value)===true){
+
+            shippingStreetNumberInput.classList.remove('is-invalid');
+            infoFaltante = false;
         }
 
         
-        if (shippingCornerStreetInput.value === "")
+        if (shippingCornerStreetInput.value === "" || hasNumber(shippingCornerStreetInput.value)===true || streetNameWithSpaces(shippingCornerStreetInput)===true)
         {
             shippingCornerStreetInput.classList.add('is-invalid');
             infoFaltante = true;
+            alert("El nombre del cruce no puede contener números ni símbolos");
+        } else {
+            shippingCornerStreetInput.classList.remove('is-invalid');
+            infoFaltante = false;
         }
-
 
         if (!metodoPagoSeleccionado)
         {
@@ -245,13 +329,13 @@ document.addEventListener("DOMContentLoaded", function(){
             let mensajeAMostrarHTML = document.getElementById("exampleModalLongTitle");
             let mensajeAMostrar = "";
 
-            if(infoFaltante == true) {
+            if(infoFaltante === true) {
 
                 mensajeAMostrar = mensaje_error;
                 mensajeAMostrarHTML.innerHTML = mensajeAMostrar;
                 document.getElementById("modalCuerpo").innerHTML = "Por favor ingrese los datos en los campos requeridos...";
 
-            } else {
+            } else if(infoFaltante === false){
 
                 mensajeAMostrar = respuesta.all.msg;
 
@@ -342,24 +426,27 @@ var tasasCambio = async function() {
   });
 }
 
-function generarNuevoProducto(array, articulos) {
+function generarNuevoProducto(productos, array) {
 
     var prod= {};
 
-    for(let i = 0; i < array.length; i++) {
-        let items = array[i];
+    for(let i = 0; i < productos.length; i++) {
+        let items = productos[i];
 
-        prod = {
-            name: items.name,
-            count: 1,   
-            unitCost: items.cost,
-            currency: items.currency,
-            src: items.imgSrc,
+        if(items.name === "Suzuki Celerio") {
+            ;
+        } else {
+
+            prod = {
+                name: items.name,
+                count: 1,   
+                unitCost: items.cost,
+                currency: items.currency,
+                src: items.imgSrc,
+            }
+        array.push(prod);
         }
-           
-        articulos.push(prod);   
     }
-   
 }
 
 var hoy = new Date();
@@ -372,8 +459,6 @@ document.getElementById("time").innerHTML = dateTime2;
 
 function removerArticulo(array) {
 
-    var arrayMini = [];
-
     for(let i = 0; i < array.length; i++) {
     
         document.getElementById(`borrar${i}`).addEventListener("click", event => {
@@ -383,86 +468,95 @@ function removerArticulo(array) {
 
             Total = Total - (prec * contador);
 
-            document.getElementById(`linea${i}`).innerHTML = "";
+            document.getElementById(`tdBorrar${i}`).parentElement.remove();
+
             actualizarCostosTotales();
 
-            arrayMini.push(array[i]);
+            arrayMini.push(array[i]); 
+            removeDuplicates(arrayMini);           
 
             mostrarRemovidosConRetorno(arrayMini);
 
-            array.splice(i);
+            var nodos = document.querySelectorAll(".varios");
+            var nodes = [...nodos];
+            console.log(nodes);
+
         });
     }
 } 
 
-function mostrarRemovidosConRetorno(array) {
+function mostrarRemovidosConRetorno(arrayMini) {
+
+    
+    let unicos = (arrayMini) => arrayMini.filter((v,i) => arrayMini.indexOf(v) === i);
+
+    arrayMini = unicos(arrayMini); 
 
     let htmlContentToAppend3 = "";
+ 
+    for(let i = 0; i < arrayMini.length; i++){ 
+        
+        let elementi = arrayMini[i];
+       
+        if(elementi != undefined) {
 
-    for(let i = 0; i < array.length; i++){ 
-        
-        let elementi = array[i];
-        
-        if(elementi.currency === "USD") {
+            if(elementi.currency === "USD" && elementi.currency != undefined) {
+                    
+                monedaProductoRemovido = "UYU";
+                costoUnitarioProductoRemovido = (elementi.unitCost * dolarAPeso);
+
+            } else if(elementi.currency === "UYU" && elementi.currency != undefined) {  
                 
-            monedaProductoRemovido = "UYU";
-            costoUnitarioProductoRemovido = (elementi.unitCost * 43);
+                monedaProductoRemovido = elementi.currency;
+                costoUnitarioProductoRemovido = elementi.unitCost;
+            }
 
-        } else if(elementi.currency === "UYU") { 
-             
-            monedaProductoRemovido = elementi.currency;
-            costoUnitarioProductoRemovido = elementi.unitCost;
+            htmlContentToAppend3 += `
+            <tr id="lineaOtro${i}">
+                <td><img src='${elementi.src}' width="85px"></td>
+                <td>${elementi.name}</td>
+                <td>${monedaProductoRemovido}</td>
+                <td class="costosOtro" id="costoUOtro${i}">${costoUnitarioProductoRemovido}</td>
+                <td><input class="form-control" style="width:60px;" id="controladorOtro${i}" type="number" value="${elementi.count}" min="1"></td>
+                <td><span class="cambiadorOtro" id="precioOtro${i}" style="font-weight:bold;">${costoUnitarioProductoRemovido * elementi.count}</span></td>
+                <td id="lineAnother${i}"><button type="button" class="btn btn-success" id="agregarOtro${i}">Agregar</button></td>    
+            </tr>
+            `;
         }
-
-        htmlContentToAppend3 += `
-        <tr id="lineaOtro${i}">
-            <td><img src='${elementi.src}' width="85px"></td>
-            <td>${elementi.name}</td>
-            <td>${monedaProductoRemovido}</td>
-            <td class="costosOtro" id="costoUOtro${i}">${costoUnitarioProductoRemovido}</td>
-            <td><input class="form-control" style="width:60px;" id="controladorOtro${i}" type="number" value="${elementi.count}" min="1"></td>
-            <td><span class="cambiadorOtro" id="precioOtro${i}" style="font-weight:bold;">${costoUnitarioProductoRemovido * elementi.count}</span></td>
-            <td><button type="button" class="btn btn-success" id="agregarOtro${i}">Agregar</button></td>
-        </tr>
-        `;
     }
     document.getElementById("mostrarArticulos-removidos").innerHTML = htmlContentToAppend3;
 
     
-    for(let i = 0; i < array.length; i++) {
+    for(let i = 0; i < arrayMini.length; i++) {
     
-        let items = array[i];
+        let items = arrayMini[i];
 
-        document.getElementById(`controladorOtro${i}`).addEventListener("change", event => {
-            
-            if(items.currency === "USD") {
+        if(items != undefined) {
 
-                monedaProductoRemovido = "UYU";
-                costoUnitarioProductoRemovido = (items.unitCost * 43);
-    
-            } else if(items.currency === "UYU") { 
-                 
-                monedaProductoRemovido = items.currency;
-                costoUnitarioProductoRemovido = items.unitCost;
-            }
-            
-            contadorRemovido = parseInt(document.getElementById(`controladorOtro${i}`).value);
-            subTotalRemovido = contadorRemovido * costoUnitarioProductoRemovido;
-            actualizarSubTotalRemovido(i); 
-        });
+            document.getElementById(`controladorOtro${i}`).addEventListener("change", event => {
+                
+                if(items.currency === "USD" && items.currency != undefined) {
 
-        document.getElementById(`agregarOtro${i}`).addEventListener("click", event => {
-            
-            var precRemovido = parseInt(document.getElementById(`costoUOtro${i}`).innerText);
-            contadorRemovido = parseInt(document.getElementById(`controladorOtro${i}`).value);
+                    monedaProductoRemovido = "UYU";
+                    costoUnitarioProductoRemovido = (items.unitCost * dolarAPeso);
+        
+                } else if(items.currency === "UYU" && items.currency != undefined) { 
+                    
+                    monedaProductoRemovido = items.currency;
+                    costoUnitarioProductoRemovido = items.unitCost;
+                }
+                
+                contadorRemovido = parseInt(document.getElementById(`controladorOtro${i}`).value);
+                subTotalRemovido = contadorRemovido * costoUnitarioProductoRemovido;
+                actualizarSubTotalRemovido(i); 
+            });
 
-            articulos.push(array[i]);
-            Total = Total + (precRemovido * contadorRemovido);
+            document.getElementById(`agregarOtro${i}`).addEventListener("click", event => {
 
-            document.getElementById(`lineaOtro${i}`).innerHTML = "";
-           
-            mostrarArticulos(articulos);
-        });
+                document.getElementById(`lineAnother${i}`).parentElement.remove();
+
+            });
+        }
     }
 }
 
@@ -472,4 +566,42 @@ function actualizarSubTotalRemovido(i){
     
     cambiarRemovido.innerHTML = monedaProductoRemovido + " " + subTotalRemovido;
     
+}
+
+function filtrados(array) {
+    
+    var filtered = array.filter(ele => ele !== undefined);
+
+    return filtered;
+}
+
+function removeDuplicates(array) {
+    return array.reduce((acc, curr) => acc.includes(curr) ? acc : [...acc, curr], []);
+};
+
+
+function hasNumber(myString) {
+
+    return /\d/.test(myString);
+}
+
+function onlyNumbers(myNumber) {
+
+    var reg = new RegExp('^[0-9]+$');
+    return reg.test(myNumber);
+}
+
+function dateFormat(dateString) {
+
+    return /^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dateString);
+}
+
+function emptyString(myString) {
+
+    return /^-?\d+\.?\d*$/.test(myString); //retorna false si la cadena está vacía
+}
+
+function streetNameWithSpaces(myString) {
+
+    return /^[a-zA-Z_\s]+$/.test(myString);
 }
